ff42d1548a23d0f0ed84a82165fa8b4050855539
LeishaCodeCraft <leisha1234567@gmail.com>
2025-06-26 16:05:55 +1000

    Initial JS for clocks; moved JS into updateTimes() function to simplify updates

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function updateTimes() {
+    // Sydney
+    let sydneyElement = document.querySelector("#sydney");
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
 
+    let nowSydney = moment().tz("Australia/Sydney");
+    sydneyDateElement.innerHTML = nowSydney.format("MMMM Do YYYY");
+    sydneyTimeElement.innerHTML =
+      nowSydney.format("h:mm:ss") +
+      " <small>" +
+      nowSydney.format("A") +
+      "</small>";
+
+    // Los Angeles
+    let laElement = document.querySelector("#los-angeles");
+    let laDateElement = laElement.querySelector(".date");
+    let laTimeElement = laElement.querySelector(".time");
+
+    let nowLA = moment().tz("America/Los_Angeles");
+    laDateElement.innerHTML = nowLA.format("MMMM Do YYYY");
+    laTimeElement.innerHTML =
+      nowLA.format("h:mm:ss") + " <small>" + nowLA.format("A") + "</small>";
+  }
+  updateTimes();
+  setInterval(updateTimes, 1000);
+});
